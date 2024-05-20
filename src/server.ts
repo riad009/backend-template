4a9ab4b01db34d7fa205dd892e040706b64edf59
riad009 <riadhossinr4@gmai.com>
2024-05-20 17:21:38 +0600

    basic server run done

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,22 @@
-import mongoose from 'mongoose';
-import app from './app';
-import config from './app/config';
+import app from "./app";
+import config from "./app/config";
+
+import mongoose from 'mongoose'
+
+main().catch(err => console.log(err));
 
 async function main() {
-  try {
+
+  try{
     await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
-  } catch (err) {
-    console.log(err);
+      console.log(` listening on port ${config.port}`);
+  });
+  } catch (error){
+    console.log("Error is", error)
   }
+
+
 }
 
-main();
